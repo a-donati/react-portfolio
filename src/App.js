823bffb66cf0827dd1d6aab61b44cf0f851dd6c5
Angela Donati <angeladonati93@gmail.com>
2022-05-11 09:56:37 -0400

    remove files, add component app.js and index.js rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import About from './containers/About';
+import Portfolio from './containers/Portfolio';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import Contact from './containers/Contact/Contact';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Sidebar />
+      <About />
+      <Footer />
     </div>
   );
 }
