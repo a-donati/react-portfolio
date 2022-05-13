1559a3fe324fc7b02eaf0f68f092a2349348fa26
Angela Donati <angeladonati93@gmail.com>
2022-05-13 17:54:22 -0400

    add devicons package and icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,61 +1,42 @@
+import DevIcon from "devicon-react-svg";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const devIconStyle = {
+    fill: "thistle",
+    width: "40px",
+};
+
 export default function About() {
     return (
-      <div>
-        <h1>Blog Page</h1>
-        <p>
-          Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-          Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-          dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-          sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-          sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-          vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-          libero. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-          lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-          Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-          in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-          bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-          est ligula suscipit velit, sed bibendum turpis dui in sapien.
-        </p>
-      </div>
+        <div>
+            <h3>About</h3>
+            <p>
+                {/* todo: put image here */}
+                I am a full stack web developer currently enrolled in University of
+                New Hampshire's Full Stack Coding Bootcamp. I am someone who loves to take their own learning and
+                skill-building
+                into their own hands, at their own pace. I aspire to work on projects that allow others to do the same.
+                Thank you for looking!
+                {/* todo: add social icons and links */}
+                <FontAwesomeIcon icon="check-square" />
+                <h4 className="mt-4">Skills </h4>
+                <DevIcon icon='html5' style={devIconStyle} />
+                <DevIcon icon='css3' style={devIconStyle} />
+                <DevIcon icon='bootstrap' style={devIconStyle} />
+                <DevIcon icon='react' style={devIconStyle} />
+                <DevIcon icon='javascript' style={devIconStyle} />
+                <DevIcon icon='nodejs' style={devIconStyle} />
+                <DevIcon icon='mongodb' style={devIconStyle} />
+                <DevIcon icon='mysql' style={devIconStyle} />
+
+            </p>
+
+
+
+
+        </div>
     );
-  }
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// // import App from './App';
-// import Main from "../Main/Main";
-// import './About.css';
-// import circleImage from '../../images/angela.png'
-
-// const About = () => {
-
-//     const aboutContent = (
-//         <div className="row px-3">
-//             <div className="col">
-//                 <p>
-//                 I am a full stack web developer currently enrolled in University of
-//                 New Hampshire's Full Stack Coding Bootcamp. I am someone who loves to take their own learning and
-//                 skill-building
-//                 into their own hands, at their own pace. I aspire to work on projects that allow others to do the same.
-//                 Thank you for looking!
-//                 <div className="social-links mt-3 text-center">
-//                 <a href="https://www.linkedin.com/in/angeladonati/" className="linkedin"><i
-//                     className="fab fa-linkedin fa-2x"></i></a>
-//                 <a href="https://github.com/a-donati" className="github"><i className="fab fa-github fa-2x"></i></a>
-//                 <a href="mailto: angeladonati93@gmail.com" className="email"><i className="fas fa-envelope fa-2x"></i></a>
-//               </div>
-//                 </p>
-                
-//             </div>
-//         </div>
-//     );
-//     return (
-//         <Main circleImage={circleImage} circleImageAlt={circleImageAlt} content ={aboutContent} title={title}/>
-//     );
-// };
-
-// export default About;
+}
