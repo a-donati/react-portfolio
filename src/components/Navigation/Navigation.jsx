e86b61570a0c61276da69ca1c454da6548c41b54
Angela Donati <angeladonati93@gmail.com>
2022-05-15 22:51:43 -0400

    update Navigation HashRouter

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,19 +3,18 @@ import { HashRouter, Link } from 'react-router-dom';
 import resume from '../../files/angela-donati-portfolio-resume.docx.pdf';
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
-// import NavDropdown from 'react-bootstrap/NavDropdown'
-import NavbarBrand from 'react-bootstrap/esm/NavbarBrand';
 import Nav from 'react-bootstrap/Nav'
 
 function Navigation () {
   return (
 <>
+
     <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="/">Angela Donati</Navbar.Brand>
+    <Navbar.Brand to="/">Angela Donati</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="/Portfolio">Portfolio</Nav.Link>
-      <Nav.Link href="/Contact">Contact</Nav.Link>
+      <Nav.Link to="/Portfolio">Portfolio</Nav.Link>
+      <Nav.Link to="/Contact">Contact</Nav.Link>
       <Nav.Link href={resume}>Resume</Nav.Link>
       
 
